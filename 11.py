94497edd008f7b643491407c875907fcbf30a402
bobchi <bobchi@163.com>
2018-08-22 08:10:26 +0800

    import PyExecJS

--- 11.py
+++ 11.py
@@ -1,6 +1,12 @@
 from spider.Fund import get_html, page1_url, get_page_count
+import execjs
 
-ret = get_html(page1_url)
+# ret = get_html(page1_url)
+#
+# print(get_page_count(ret))
 
-print(get_page_count(ret))
+js_engine = execjs.get()
+print(js_engine.eval("1+33"))
+# print(ret)
+
 
--- spider/Fund.py
+++ spider/Fund.py
@@ -2,7 +2,7 @@
 
 from bs4 import BeautifulSoup
 from urllib import request
-import chardet # 自动检测编码的包
+import chardet  # 自动检测编码的包
 from bs4 import BeautifulSoup
 
 page1_url = 'http://fund.eastmoney.com/fund.html'
@@ -10,11 +10,11 @@ page1_url = 'http://fund.eastmoney.com/fund.html'
 def get_html(page_url):
     response = request.urlopen(page_url)
     raw_html = response.read()
-    get_encoding = chardet.detect(raw_html)['encoding'] # 检测网页编码
+    get_encoding = chardet.detect(raw_html)['encoding']  # 检测网页编码
     return raw_html.decode(get_encoding)
 
 def get_page_count(html):
-    soup  = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, 'html.parser')
     page_count = soup.find('div', id='pager').find('span', 'nv').get_text()
     return (''.join(filter(str.isdigit, page_count)))   # 提取字符串中的数字
 
