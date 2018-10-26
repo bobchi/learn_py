d2de241625aafe6ebcef1e8845b439d722c92f5c
bobchi <bobchi@163.com>
2018-10-26 09:58:00 +0800

    31 add

--- 31.py
+++ 31.py
@@ -11,7 +11,8 @@ import jieba.analyse
 # 过滤停用词手工写法
 str1 = '据说程序员是一个很有前途的工作，我要好好学习，将来做程序员。前两天我在网上买了一本开发方面的书，卖家发的顺丰快递，快递小哥2天就给送到，这几天我正式开始写代码学习了。'
 lst = jieba.cut(str, cut_all=False)
-stop = [line.decode('utf-8').strip() for line in open('./files/stopwords.txt','rb').readlines()]
-print(stop)
+stop = [line.decode('utf-8').strip() for line in open('./files/stopwords.txt', 'rb').readlines()]
+tags = jieba.analyse.set_stop_words('./files/stopwords.txt')
+print(tags)
 
 
